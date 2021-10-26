74707b74db115887b7085994a03179b14fc30a5d
deigo siahaan <deigosiahaan@gmail.com>
2021-10-26 22:22:06 +0700

    update controller and routes

--- controller.js
+++ controller.js
@@ -7,3 +7,15 @@ var connection = require('./koneksi');
 exports.index = function(req, res) {
     respones.ok("Rest API berjalan", res);
 }
+
+// menampilkan semua data
+exports.tampilData = function(req, res){
+    connection.query('SELECT * FROM mahasiswa', function(error, rows, fileds){
+        if(error) {
+            connection.log(error);
+        }
+        else {
+            respones.ok(rows, res);
+        }
+    })
+}
